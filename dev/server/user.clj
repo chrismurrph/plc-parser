7e777d661845ab01c219f3bf63bd88ffca9af4f7
Chris Murphy <chris@strandz.org>
2016-08-24 16:54:02 +1000

    Having real trouble adding datatype between two newlines

--- dev/server/user.clj
+++ dev/server/user.clj
@@ -1,6 +1,7 @@
 (ns user
   (:require [clojure.string :as str]
-            [instaparse.core :as insta]))
+            [instaparse.core :as insta]
+            [clojure.pprint :as pp]))
 
 (defn r []
   (require 'user :reload))
@@ -9,11 +10,14 @@
 
 (def input (slurp "version_assign.txt"))
 
-(defn parse []
+(defn parse-it []
   (let [parser (insta/parser ebnf)
-        res (parser input)]
-    res))
+        res (insta/parses parser input)
+        _ (assert (seq res) (str "Empty list"))
+        choices (count res)
+        _ (assert (= choices 1))]
+    (first res)))
 
 (defn x []
-  (let [res (parse)]
-    (clojure.pprint/pprint res)))
+  (let [res (parse-it)]
+    (pp/pprint res)))
