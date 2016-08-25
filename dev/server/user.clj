29a36229ab8220afbc7ea5c0ee2f67bba98d71f7
Chris Murphy <chris@strandz.org>
2016-08-25 22:39:00 +0930

    About to skip add on instructions as they will be easier later

--- dev/server/user.clj
+++ dev/server/user.clj
@@ -11,20 +11,22 @@
 (def input (slurp "version_assign.txt"))
 
 (defn parse-it []
-  (let [parser (insta/parser ebnf)
-        xs (insta/parses parser input)
+  (let [my-parser (insta/parser ebnf)
+        xs (insta/parses my-parser input)
         num-choices (count xs)
-        _ (when (zero? num-choices) (println "insta/parses might be able to be corrected"))
-        _ (when (> num-choices 1) (println (str "insta/parses shows more than one way to parse: " num-choices)))
-        _ (when (= 1 num-choices) (println "insta/parses shows one choice, so all good"))
+        msg (cond
+              (zero? num-choices) "insta/parses might be able to be corrected"
+              (> num-choices 1) (str "insta/parses shows more than one way to parse: " num-choices)
+              (= 1 num-choices) "insta/parses shows one choice, so all good")
         res (cond
               ;; Just fix there being more than one rather than show them all
               ;(> num-choices 1) xs
               (= num-choices 1) (first xs)
-              (zero? num-choices) (insta/parse parser input))
+              (zero? num-choices) (insta/parse my-parser input))
         _ (assert res)]
-    res))
+    [msg res]))
 
 (defn x []
-  (let [res (parse-it)]
-    (pp/pprint res)))
+  (let [[msg res] (parse-it)]
+    (pp/pprint res)
+    (println msg)))
