c076ab3130470c35c14e708985aa0dc7e746317f
Chris Murphy <chris@strandz.org>
2016-08-25 15:05:02 +1000

    Just need to make list of custom types - will do with visual inspection

--- dev/server/user.clj
+++ dev/server/user.clj
@@ -14,10 +14,14 @@
   (let [parser (insta/parser ebnf)
         xs (insta/parses parser input)
         num-choices (count xs)
-        _ (when (zero? num-choices) (println "insta/parses is no good but can be corrected"))
-        res (case num-choices
-              1 (first xs)
-              0 (insta/parse parser input))
+        _ (when (zero? num-choices) (println "insta/parses might be able to be corrected"))
+        _ (when (> num-choices 1) (println (str "insta/parses shows more than one way to parse: " num-choices)))
+        _ (when (= 1 num-choices) (println "insta/parses shows one choice, so all good"))
+        res (cond
+              ;; Just fix there being more than one rather than show them all
+              ;(> num-choices 1) xs
+              (= num-choices 1) (first xs)
+              (zero? num-choices) (insta/parse parser input))
         _ (assert res)]
     res))
 
